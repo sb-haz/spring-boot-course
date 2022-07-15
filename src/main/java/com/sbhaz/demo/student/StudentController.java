67f72c5025a6f3904d7dde531fb182422b3f8578
sb-haz <sb_haz@icloud.com>
2022-07-15 21:42:28 +0100

    Initial commit

---
+++ src/main/java/com/sbhaz/demo/student/StudentController.java
@@ -0,0 +1,25 @@
+package com.sbhaz.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+
+    private final StudentService service;
+
+    @Autowired
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return service.getStudents();
+    }
+}
